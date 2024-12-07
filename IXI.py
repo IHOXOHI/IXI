d0c5269903edbb119b0e4c682ce2f431626e2fa4
IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>
2024-12-07 12:47:34 +0100

    Update IXI.py
    finally the best that I have found to calibrate correctly from a script prepared for that.

--- IXI.py
+++ IXI.py
@@ -1,64 +1,31 @@
+import uasyncio
 from pyb import RTC
-import struct
 
 rtc = RTC()
 
-results = []
+rtc.calibration(-171)
 
-class CALI:
-    def __init__(self,rtc_cal=0,jump=1):
-        ###variables modificables
-        self.rtc_cal = rtc_cal
-        self.jump = jump
-        ###variables
-        self.nb_cycles = 0
-        self.n_period = 0
-        self.t_mem = 0
-        self.results = results
-        ###functions
-        CalState = self.CalState
-        CalChange = self.CalChange
-        CalJump = self.CalJump
-        Cycle = self.Cycle
-        PrintResults = self.PrintResults
+n = 0
+t_mem = 0
 
-    def CalState(self):
-        return self.rtc_cal
-
-    def CalChange(self,rtc_cal):
-        rtc.calibration(rtc_cal)
-        self.rtc_cal = rtc_cal
-        return self.rtc_cal
-
-    def CalJump(self):
-        return self.jump
-
-    def StartCycle(self):
-        rtc.calibration(self.rtc_cal)
-        t_mem = rtc.datetime()[7]
-        self.t_mem = t_mem
-
-    def Cycle(self):
-        t = rtc.datetime()[7]
-        if self.n_period == 1:
-            self.nb_cycles += 1
-            if t != self.t_mem:
-                result = (self.rtc_cal,self.nb_cycles)
-                self.results.append(result)
-                if t > self.t_mem:
-                    self.rtc_cal += self.jump
-                else:
-                    self.rtc_cal -= self.jump
-                self.CalChange(self.rtc_cal)
-                self.nb_cycles = 0
-                self.t_mem = t
-                self.n_period = 0
+async def main():
+    global n, t_mem
+    while 1:
+        if n == 0:
+            t = rtc.datetime()[7]
+            t_mem = t
+            n = 1
+        if n == 1:
+            t = rtc.datetime()[7]
+            if t != t_mem:
+                n = 2
+                t_mem = t
         else:
-            if t != self.t_mem:
-                self.t_mem = t
-                self.n_period = 1
+            n+=1
+            t = rtc.datetime()[7]
+            if t != t_mem:
+                print(n)
+                break
+        await uasyncio.sleep_ms(1000)
 
-        return self.nb_cycles
-
-    def PrintResults(self):
-        return self.results
+uasyncio.run(main())

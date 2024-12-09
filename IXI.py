cb93f34ce8fd74fb63607baac5255488bdbc4a05
IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>
2024-12-09 21:21:52 +0100

    Update IXI.py
    better...

--- IXI.py
+++ IXI.py
@@ -11,19 +11,23 @@ t_mem = 0
 async def main():
     global n, t_mem
     while 1:
-        if n == 0:
-            t = rtc.datetime()[7]
-            t_mem = t
-            n = 1
         if n == 1:
             t = rtc.datetime()[7]
             if t != t_mem:
                 n = 2
                 t_mem = t
+        if n == 0:
+            t = rtc.datetime()[7]
+            t_mem = t
+            n = 1
         else:
             n+=1
             t = rtc.datetime()[7]
             if t != t_mem:
+                if t > t_mem:
+                    print("+")
+                else:
+                    print("-")
                 print(n)
                 break
         await uasyncio.sleep_ms(1000)
